312d4bf7be6bf2d197174089840bf9afb7c33cbf
markchck <markchck@gmail.com>
2022-11-08 20:59:37 +0900

    launch.json 수정

--- proxy.c
+++ proxy.c
@@ -1,70 +1,53 @@
 #include <stdio.h>
-#include "csapp.c"
-
+#include "csapp.h"
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
 
 /* You won't lose style points for including this long line in your code */
-static const char *user_agent_hdr =
-    "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
-    "Firefox/10.0.3\r\n";
+static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+static const char *conn_hdr = "Connection: close\r\n";
+static const char *prox_hdr = "Proxy-Connection: close\r\n";
+static const char *host_hdr_format = "Host: %s\r\n";
+static const char *requestlint_hdr_format = "GET %s HTTP/1.0\r\n";
+static const char *endof_hdr = "\r\n";
 
-/* PROTOTYPES */
+static const char *connection_key = "Connection";
+static const char *user_agent_key= "User-Agent";
+static const char *proxy_connection_key = "Proxy-Connection";
+static const char *host_key = "Host";
 
-int main(int argc, char **argv) {
-    int listenfd, connfd;
-    char hostname[MAXLINE], port[MAXLINE];
-    socklen_t clientlen;
-    struct sockaddr_storage clientaddr;
-    
-    /*check command-line args*/
+void doit(int connfd);
+void parse_uri(char *uri,char *hostname,char *path,int *port);
+void build_http_header(char *http_header,char *hostname,char *path,int port,rio_t *client_rio);
+int connect_endServer(char *hostname,int port,char *http_header);
+
+int main(int argc,char **argv)
+{
+    int listenfd,connfd;
+    socklen_t  clientlen;
+    char hostname[MAXLINE],port[MAXLINE];
+
+    struct sockaddr_storage clientaddr;/*generic sockaddr struct which is 28 Bytes.The same use as sockaddr*/
+
     if(argc != 2){
-        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        fprintf(stderr,"usage :%s <port> \n",argv[0]);
         exit(1);
     }
+
     listenfd = Open_listenfd(argv[1]);
     while(1){
         clientlen = sizeof(clientaddr);
-        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
-        Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
-        printf("Accepted connection from(%s, %s)\n", hostname, port);
-        doit(connfd);
-        Close(connfd);
+        connfd = Accept(listenfd,(SA *)&clientaddr,&clientlen);
+
+        /*print accepted message*/
+        Getnameinfo((SA*)&clientaddr,clientlen,hostname,MAXLINE,port,MAXLINE,0);
+        printf("Accepted connection from (%s %s).\n",hostname,port);
+
+        /*sequential handle the client transaction*/
+        // doit(connfd);
+
+        // Close(connfd);
     }
-    printf("%s", user_agent_hdr);
     return 0;
 }
-
-void doit(int connfd){
-    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
-    /*store the request line arguments*/
-    char hostname[MAXLINE], path[MAXLINE];
-    int port;
-
-    rio_t rio;
-    /*Read request line and headers*/
-    Rio_readinitb(&rio,connfd);
-    Rio_readlineb(&rio, buf, MAXLINE);
-    printf("Request headers:\n");
-    printf("%s", buf);
-    sscanf(buf, "%s %s %s", method, uri, version);
-    
-    if (strcasecmp(method, "GET")){
-        printf("Proxy does not implement the method");
-        return;
-    }
-
-    /*parse the uri to get hostanme, file path, port*/
-    parse_uri(uri, hostname, path, &port);
-
-    /*build the http header which will send to the end server*/
-    //여기서 end server는 프록시가 대리하고 있는 서버
-    // build_http_header();
-
-}
-
-// parse_uri(char *uri, char *hostname, char *path, int *port){
-    
-// }
-
